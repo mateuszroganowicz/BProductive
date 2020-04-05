d2a7963cddf327d4be1a3852dc0e15ef6af2cfb6
Mateusz <matiroganowicz@gmail.com>
2020-04-05 23:12:50 +0200

    timer working

--- app/src/main/java/com/example/bproductive3/ui/timer/TimerFragment.java
+++ app/src/main/java/com/example/bproductive3/ui/timer/TimerFragment.java
@@ -1,35 +1,173 @@
 package com.example.bproductive3.ui.timer;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+
 
 import com.example.bproductive3.R;
 
-public class TimerFragment extends Fragment {
 
-    private TimerViewModel timerViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        timerViewModel =
-                ViewModelProviders.of(this).get(TimerViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_timer, container, false);
-        final TextView textView = root.findViewById(R.id.text_timer);
-        timerViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+public class TimerFragment extends Fragment  {
+
+    private final static long start_time = 6000; //In miliseconds
+    private final static long break_time = 16000; //In miliseconds
+    private long timeInMs = start_time;
+    private TextView textCountDown;
+    private ImageButton ButtonStartPause;
+    private ImageButton ButtonRestart;
+    private ImageView Circle0;
+    private ImageView Circle1;
+    private ImageView Circle2;
+    private ImageView Circle3;
+
+
+    private CountDownTimer countDownTimer;
+    private boolean timerRun;
+    private boolean timeToBreak = true;
+    private int session = 0;
+    private int totalSessions = 4;
+    View view;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_timer, container, false);
+
+        textCountDown = view.findViewById(R.id.countdown_text);
+        ButtonStartPause = view.findViewById(R.id.button_start);
+        ButtonRestart = view.findViewById(R.id.button_reset);
+        Circle0 = view.findViewById(R.id.circle_0);
+        Circle1 = view.findViewById(R.id.circle_1);
+        Circle2 = view.findViewById(R.id.circle_2);
+        Circle3 = view.findViewById(R.id.circle_3);
+
+
+        ButtonStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                startStop();
             }
         });
-        return root;
+
+        ButtonRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset();
+            }
+        });
+
+        return view;
     }
+
+    public void startStop(){
+        if(timerRun){
+            stopTimer();
+        }
+        else{
+            startTimer();
+        }
+    }
+
+    public void startTimer(){
+        countDownTimer = new CountDownTimer(timeInMs, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeInMs = millisUntilFinished;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+        ButtonStartPause.setImageResource(R.drawable.ic_pause);
+
+        timerRun = true;
+
+    }
+
+    public void stopTimer(){
+        countDownTimer.cancel();
+        ButtonStartPause.setImageResource(R.drawable.ic_play);
+        timerRun = false;
+    }
+
+    public void updateTimer(){
+        int minutes = (int) timeInMs / 60000;
+        int seconds = (int) timeInMs % 60000 / 1000;
+
+        String timeLeft = null;
+        if(minutes < 10) timeLeft = "0";
+        timeLeft += minutes;
+        timeLeft += ":";
+        if(seconds < 10) timeLeft += "0";
+        timeLeft += seconds;
+
+        textCountDown.setText(timeLeft);
+        if(timeLeft.equals("00:00") && session < totalSessions){
+
+            breakTime();
+        }
+        else if(timeLeft.equals("00:00") && session == totalSessions){
+            reset();
+        }
+
+    }
+
+    public void reset(){
+        if(timerRun) {
+            countDownTimer.cancel();
+            timeInMs = start_time;
+            ButtonStartPause.setImageResource(R.drawable.ic_play);
+            timerRun = false;
+            updateTimer();
+        }
+        else{}
+    }
+
+    public void breakTime(){
+        if(timeToBreak) {
+            timeInMs = break_time;
+            startTimer();
+            session++;
+            System.out.println("Number of sessions: " + session);
+            if(session == 1){
+                Circle0.setImageResource(R.drawable.ic_moon);
+            }
+            else if(session == 2){
+
+                    Circle1.setImageResource(R.drawable.ic_moon);
+
+            }
+            else if(session == 3){
+
+                Circle2.setImageResource(R.drawable.ic_moon);
+
+            }
+            else if(session == 4){
+                Circle3.setImageResource(R.drawable.ic_moon);
+            }
+
+
+            timeToBreak = false;
+        }
+        else{
+            timeInMs = start_time;
+            startTimer();
+            timeToBreak = true;
+        }
+
+    }
+
 }
