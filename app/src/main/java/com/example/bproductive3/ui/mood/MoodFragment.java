e4944d5464863a1b0ef7a26abb937e26ece38fd0
ProudBloom <jakub-sztompka@wp.pl>
2020-04-07 17:51:51 +0200

    MoodFragment.java updated

--- app/src/main/java/com/example/bproductive3/ui/mood/MoodFragment.java
+++ app/src/main/java/com/example/bproductive3/ui/mood/MoodFragment.java
@@ -1,10 +1,16 @@
 package com.example.bproductive3.ui.mood;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,22 +18,42 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.bproductive3.MainActivity;
 import com.example.bproductive3.R;
+import com.example.bproductive3.Util;
 
-public class MoodFragment extends Fragment {
+import java.util.Objects;
 
+public class MoodFragment extends Fragment
+{
     private MoodViewModel moodViewModel;
+    private View moodView;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        moodViewModel =
-                ViewModelProviders.of(this).get(MoodViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        moodViewModel = ViewModelProviders.of(this).get(MoodViewModel.class);
         View root = inflater.inflate(R.layout.fragment_mood, container, false);
-        final TextView textView = root.findViewById(R.id.text_mood);
-        moodViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        ImageButton happyBtt = root.findViewById(R.id.button_happy);
+        ImageButton sadBtt = root.findViewById(R.id.button_sad);
+
+        happyBtt.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v)
+            {
+                Toast.makeText(getActivity(),"You are Happy!",Toast.LENGTH_SHORT).show();
+                Util.changeToTheme(getActivity(), Util.THEME_HAPPY);
+            }
+        });
+
+        sadBtt.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(getActivity(),"You are Sad :(",Toast.LENGTH_SHORT).show();
+                Util.changeToTheme(getActivity(), Util.THEME_SAD);
             }
         });
         return root;
